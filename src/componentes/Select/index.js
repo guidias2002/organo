4514ee40e3573b8bb3020528e819d885fab3f03c
Guilherme <guidias200220@gmail.com>
2023-04-18 00:05:58 -0300

    first commit

---
+++ src/componentes/Select/index.js
@@ -0,0 +1,17 @@
+import './Select.css'
+
+const Select = (props) => {
+    return (
+        <div className="select">
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default Select
